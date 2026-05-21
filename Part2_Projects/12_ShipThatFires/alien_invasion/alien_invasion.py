8a562b913d56355612296588b58f7d7ff587eb22
SevenWoos <swenwu96@gmail.com>
2026-05-21 11:20:00 -0700

    Fixed indenting in alien_invasion.py.

--- Part2_Projects/12_ShipThatFires/alien_invasion/alien_invasion.py
+++ Part2_Projects/12_ShipThatFires/alien_invasion/alien_invasion.py
@@ -6,35 +6,35 @@ from settings import Settings
 from ship import Ship
 
 class AlienInvasion:
-    """Overall class to manage game assets and behavior."""
-    
-    def __init__(self):
-       """Initialize the game, and create game resources."""
-       pygame.init()
+   """Overall class to manage game assets and behavior."""
+   
+   def __init__(self):
+      """Initialize the game, and create game resources."""
+      pygame.init()
 
-       self.clock = pygame.time.Clock()
-       self.settings = Settings()
+      self.clock = pygame.time.Clock()
+      self.settings = Settings()
 
-       self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
-       pygame.display.set_caption("Alien Invasion")
+      self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+      pygame.display.set_caption("Alien Invasion")
 
-       self.ship = Ship(self)
+      self.ship = Ship(self)
 
-       # Set the background color.
-       self.bg_color = (230, 230, 230)
+      # Set the background color.
+      self.bg_color = (230, 230, 230)
 
-    def run_game(self):
-        """Start the main loop for the game."""
-        while True:
-          self._check_events()
-          self.ship.update()
-          self._update_screen()
-          self.clock.tick(60)
+   def run_game(self):
+      """Start the main loop for the game."""
+      while True:
+         self._check_events()
+         self.ship.update()
+         self._update_screen()
+         self.clock.tick(60)
 
-    def _check_events(self):
-       """Respond to keypresses and mouse events."""
-       # Watch for keyboard and mouse events.
-       for event in pygame.event.get():
+   def _check_events(self):
+      """Respond to keypresses and mouse events."""
+      # Watch for keyboard and mouse events.
+      for event in pygame.event.get():
          if event.type == pygame.QUIT:
             sys.exit()
 
@@ -44,25 +44,26 @@ class AlienInvasion:
          elif event.type == pygame.KEYUP:
             self._check_keyup_events(event)
 
-    def _check_keydown_events(self, event):
-        """Respond to keypresses."""
-        if event.key == pygame.K_RIGHT:
-            self.ship.moving_right = True
-        elif event.key == pygame.K_LEFT:
-            self.ship.moving_left = True
-   
-    def _check_keyup_events(self, event):
-       if event.key == pygame.K_RIGHT:
-            self.ship.moving_right = False
-       elif event.key == pygame.K_LEFT:
-            self.ship.moving_left = False
+   def _check_keydown_events(self, event):
+      """Respond to keypresses."""
+      if event.key == pygame.K_RIGHT:
+         self.ship.moving_right = True
+      elif event.key == pygame.K_LEFT:
+         self.ship.moving_left = True
 
-    def _update_screen(self):
+
+   def _check_keyup_events(self, event):
+      if event.key == pygame.K_RIGHT:
+         self.ship.moving_right = False
+      elif event.key == pygame.K_LEFT:
+         self.ship.moving_left = False
+
+   def _update_screen(self):
       """Update images on the screen, and flip to the new screen."""
       # Redraw the screen during each pass through the loop.
       self.screen.fill(self.settings.bg_color)
       self.ship.blitme()
-      
+
       # Make the most recently drawn screen available.
       pygame.display.flip()
 
@@ -70,4 +71,3 @@ if __name__ == '__main__':
    # Make a game instance, and run the game.
    ai = AlienInvasion()
    ai.run_game()
-          
