2254e9362ae949280ba2f496457317b56bb6c77e
SevenWoos <swenwu96@gmail.com>
2026-05-21 12:48:22 -0700

    Create Rocket Pygame window with sprite.

---
+++ Part2_Projects/12_ShipThatFires/rocket/settings.py
@@ -0,0 +1,11 @@
+class Settings:
+  """A class to store all settings for the Rocket game."""
+  
+  def __init__(self):
+    """Initialize the game's settings."""
+    # Screen settings
+    self.screen_width = 1200
+    self.screen_height = 800
+    self.bg_color = (135, 206, 235)
+    self.rocket_speed = 1.5
+    
