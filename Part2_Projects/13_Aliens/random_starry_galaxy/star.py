d7bc0ef5945df867b5f1e121b6df728a66404036
SevenWoos <swenwu96@gmail.com>
2026-05-24 18:58:14 -0700

    Create a Random Starry Galaxy pygame window with x and y offsets.

---
+++ Part2_Projects/13_Aliens/random_starry_galaxy/star.py
@@ -0,0 +1,22 @@
+import pygame
+from pygame.sprite import Sprite
+
+class Star(Sprite):
+  """A class to represent a single star in the galaxy."""
+  
+  def __init__(self, sg_game):
+    """Initialize the star and set its starting position."""
+    super().__init__()
+    self.screen = sg_game.screen
+    
+    # Load the star image and get its rect.
+    self.image = pygame.image.load('images/star.png').convert_alpha()
+    self.image = pygame.transform.scale(self.image, (40, 40))
+    self.rect = self.image.get_rect()
+    
+    # Start each new star near the top left of the screen.
+    self.rect.x = self.rect.width
+    self.rect.y = self.rect.height
+    
+    # Store the star's exact horizontal position.
+    self.x = float(self.rect.x)
