9dbc135b441b973154081f0c4bf313c74b552c19
SevenWoos <swenwu96@gmail.com>
2026-05-26 17:02:13 -0700

    Responding to alien-ship collisions. Create GameStats to teack game stats. Reset fleet and center ship if ship is hit.

--- Part2_Projects/13_Aliens/alien_invasion/alien_invasion.py
+++ Part2_Projects/13_Aliens/alien_invasion/alien_invasion.py
@@ -1,8 +1,10 @@
 import sys
+from time import sleep
 
 import pygame
 
 from settings import Settings
+from game_stats import GameStats
 from ship import Ship
 from bullet import Bullet
 from alien import Alien
@@ -19,6 +21,9 @@ class AlienInvasion:
     
     self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
+    
+    # Create an instance to store game statistics.
+    self.stats = GameStats(self)
     
     self.ship = Ship(self)
     # Group that holds the bullets, and allows you to manage them.
@@ -104,7 +109,7 @@ class AlienInvasion:
     
     # Look for alien-ship collisions.
     if pygame.sprite.spritecollideany(self.ship, self.aliens):
-      print("Ship hit!")
+      self._ship_hit()
         
   def _create_alien(self, x_position, y_position):
     """Create an alien and place it in the row."""
@@ -144,6 +149,22 @@ class AlienInvasion:
       alien.rect.y += self.settings.fleet_drop_speed
     self.settings.fleet_direction *= -1
       
+  def _ship_hit(self):
+    """Respond to the ship being hit by an alien."""
+    # Decrement ships left.
+    self.stats.ships_left -= 1
+    
+    # Get rid of any remaining bullets and aliens.
+    self.bullets.empty()
+    self.aliens.empty()
+    
+    # Create a new fleet and center the ship.
+    self._create_fleet()
+    self.ship.center_ship()
+    
+    # Pause
+    sleep(0.5)
+  
   def _update_screen(self):
     """Update images on the screen, and flip to the new screen."""
     # Redraw the screen during each pass through the loop.
--- Part2_Projects/13_Aliens/alien_invasion/settings.py
+++ Part2_Projects/13_Aliens/alien_invasion/settings.py
@@ -3,11 +3,14 @@ class Settings:
   
   def __init__(self):
     """Initialize the game's settings."""
-    # Screen settings.
+    # Screen Settings.
     self.screen_width = 1200
     self.screen_height = 800
     self.bg_color = (230, 230, 230)
+    
+    # Ship Settings.
     self.ship_speed = 1.5
+    self.ship_limit = 3
     
     # Bullet Settings.
     self.bullet_speed = 2.5
@@ -18,7 +21,7 @@ class Settings:
     
     # Alien Settings.
     self.alien_speed = 1.0
-    self.fleet_drop_speed = 10
+    self.fleet_drop_speed = 30
     # fleet direction of 1 represents right; -1 represents left.
     self.fleet_direction = -1
     
