d5dcb1bda9a0d42f1f71fba874fecb168c5ce0c1
SevenWoos <swenwu96@gmail.com>
2026-05-26 17:17:17 -0700

    Dealing with aliens that reach the bottom of the screen. Run the ship collided function.

--- Part2_Projects/13_Aliens/alien_invasion/alien_invasion.py
+++ Part2_Projects/13_Aliens/alien_invasion/alien_invasion.py
@@ -110,6 +110,9 @@ class AlienInvasion:
     # Look for alien-ship collisions.
     if pygame.sprite.spritecollideany(self.ship, self.aliens):
       self._ship_hit()
+      
+    # Look for aliens hitting the bottom of the screen.
+    self._check_aliens_bottom()
         
   def _create_alien(self, x_position, y_position):
     """Create an alien and place it in the row."""
@@ -164,6 +167,14 @@ class AlienInvasion:
     
     # Pause
     sleep(0.5)
+    
+  def _check_aliens_bottom(self):
+    """Check if any aliens have reached the bottom of the screen."""
+    for alien in self.aliens.sprites():
+      if alien.rect.bottom >= self.settings.screen_height:
+        # Treat this the same as if the ship got it.
+        self._ship_hit()
+        break
   
   def _update_screen(self):
     """Update images on the screen, and flip to the new screen."""
