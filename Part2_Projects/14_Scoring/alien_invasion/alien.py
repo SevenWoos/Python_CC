79dbad10cf4b4809e97353be28b53605a1167f45
SevenWoos <swenwu96@gmail.com>
2026-06-01 12:41:49 -0700

    Created check_edges function in Alien class.

--- Part2_Projects/14_Scoring/alien_invasion/alien.py
+++ Part2_Projects/14_Scoring/alien_invasion/alien.py
@@ -20,6 +20,11 @@ class Alien(Sprite):
     # Store the alien's exact horizontal position
     self.x = float(self.rect.x)
     
+  def check_edges(self):
+    """Return True if alien is at edge of screen."""
+    screen_rect = self.screen.get_rect()
+    return (self.rect.right >= screen_rect.right) or (self.rect.left <= 0)
+    
   def update(self):
     """Move the alien left or right."""
     self.x += self.settings.alien_speed * self.settings.fleet_direction
