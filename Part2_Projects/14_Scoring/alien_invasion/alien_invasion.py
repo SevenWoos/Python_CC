954f25c094814c821bccbe2fefccffa37d014f99
SevenWoos <swenwu96@gmail.com>
2026-05-31 22:58:26 -0700

    Make the pygame screen.

---
+++ Part2_Projects/14_Scoring/alien_invasion/alien_invasion.py
@@ -0,0 +1,53 @@
+import sys
+import pygame
+
+from settings import Settings
+
+class AlienInvasion:
+  """Overall class to manage game assets and behavior."""
+  
+  def __init__(self):
+    """Initialize the game, and create game resources."""
+    pygame.init()
+    
+    self.clock = pygame.time.Clock()
+    self.settings = Settings()
+    
+    self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+    self.screen_rect = self.screen.get_rect()
+    pygame.display.set_caption("Alien Invasion")
+    
+  def run_game(self):
+    while True:
+      self._check_events()
+      self._update_screen()
+      self.clock.tick(60)
+      
+  def _check_events(self):
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+        sys.exit()
+      elif event.type == pygame.KEYDOWN:
+        self._check_events_keydown(event)
+      elif event.type == pygame.KEYUP:
+        self._check_events_keyup(event)
+  
+  def _check_events_keydown(self, event):
+    """Respond to keypresses."""
+    if event.key == pygame.K_q:
+      sys.exit()
+  
+  def _check_events_keyup(self, event):
+    return None
+  
+  def _update_screen(self):
+    """Update images on the screen, and flip to the new screen."""
+    self.screen.fill(self.settings.bg_color)
+    
+    # Make the most recently drawn screen available.
+    pygame.display.flip()
+    
+
+if __name__ == '__main__':
+  ai = AlienInvasion()
+  ai.run_game()
