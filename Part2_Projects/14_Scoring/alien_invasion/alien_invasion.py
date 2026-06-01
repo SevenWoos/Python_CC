ca4569001ea76fac918b385e4562e54d4eb6394e
SevenWoos <swenwu96@gmail.com>
2026-06-01 16:49:25 -0700

    Hide the mouse cursor after starting the game. Make it reappear when the game ends.

--- Part2_Projects/14_Scoring/alien_invasion/alien_invasion.py
+++ Part2_Projects/14_Scoring/alien_invasion/alien_invasion.py
@@ -81,6 +81,9 @@ class AlienInvasion:
       # Create a new fleet and center the ship.
       self._create_fleet()
       self.ship.center_ship()
+      
+      # Hide the mosue cursor
+      pygame.mouse.set_visible(False)
   
   def _check_events_keydown(self, event):
     """Respond to keypresses."""
@@ -197,6 +200,7 @@ class AlienInvasion:
       sleep(0.5)
     else:
       self.game_active = False
+      pygame.mouse.set_visible(True)
       
   def _check_aliens_bottom(self):
     """Check if any aliens have reached the bottom of the screen."""
