b0b7692dd280b3c32f8ab667b280cf574b784d95
SevenWoos <swenwu96@gmail.com>
2026-06-03 13:33:52 -0700

    Updating the score as aliens are hit.

--- Part2_Projects/14_Scoring/alien_invasion/alien_invasion.py
+++ Part2_Projects/14_Scoring/alien_invasion/alien_invasion.py
@@ -136,6 +136,10 @@ class AlienInvasion:
     # Remove any bullets and aliens that have collided.
     collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
     
+    if collisions:
+      self.stats.score += self.settings.alien_points
+      self.sb.prep_score()
+    
     # Respawn new fleet when one is completely destroyed.
     if not self.aliens:
       # Destroy existing bullets and create new fleet.
--- Part2_Projects/14_Scoring/alien_invasion/settings.py
+++ Part2_Projects/14_Scoring/alien_invasion/settings.py
@@ -38,6 +38,9 @@ class Settings:
     # fleet direction of 1 represents right; -1 represents left.
     self.fleet_direction = -1
     
+    # Scoring Settings
+    self.alien_points = 20
+    
   def increase_speed(self):
     """Increase speed settings."""
     self.ship_speed *= self.speedup_scale
