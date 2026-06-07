e913da09c8131812c6f5196561bd41eab0714808
SevenWoos <swenwu96@gmail.com>
2026-06-06 18:38:52 -0700

    Disabling scientific notation for large numbers on the y-axis.

--- Part2_Projects/15_GeneratingData/scatter_squares.py
+++ Part2_Projects/15_GeneratingData/scatter_squares.py
@@ -16,6 +16,9 @@ ax.set_title("Square Numbers", fontsize=24)
 ax.set_xlabel("Value", fontsize=14)
 ax.set_ylabel("Square of Value", fontsize=14)
 
+# Disabling scientific notation for large numbers.
+ax.ticklabel_format(style='plain')
+
 # Set size of tick labels.
 ax.tick_params(labelsize=14)
 
