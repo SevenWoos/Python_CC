fb5196bbc6700a4c3a073507e78fa3faa522a6e8
SevenWoos <swenwu96@gmail.com>
2026-06-06 18:39:32 -0700

    Definining custom colors in the scatterplot.

--- Part2_Projects/15_GeneratingData/scatter_squares.py
+++ Part2_Projects/15_GeneratingData/scatter_squares.py
@@ -9,7 +9,7 @@ y_values = [x**2 for x in x_values]
 
 plt.style.use('seaborn-v0_8')
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, s=10)
+ax.scatter(x_values, y_values, color='red', s=10)
 
 # Set chart title and label axes.
 ax.set_title("Square Numbers", fontsize=24)
@@ -17,7 +17,7 @@ ax.set_xlabel("Value", fontsize=14)
 ax.set_ylabel("Square of Value", fontsize=14)
 
 # Disabling scientific notation for large numbers.
-ax.ticklabel_format(style='plain')
+# ax.ticklabel_format(style='plain')
 
 # Set size of tick labels.
 ax.tick_params(labelsize=14)
