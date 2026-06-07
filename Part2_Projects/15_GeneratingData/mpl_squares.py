0911de68a437f437ac73cd0b32ac596af64e2c6b
SevenWoos <swenwu96@gmail.com>
2026-06-06 18:02:41 -0700

    Using built-in styles.

--- Part2_Projects/15_GeneratingData/mpl_squares.py
+++ Part2_Projects/15_GeneratingData/mpl_squares.py
@@ -3,6 +3,9 @@ import matplotlib.pyplot as plt
 input_values = [1, 2, 3, 4, 5]
 squares = [1, 4, 9, 16, 25]
 
+# Trying different styles.
+plt.style.use('seaborn-v0_8')
+# plt.style.use('fivethirtyeight')
 fig, ax = plt.subplots()
 ax.plot(input_values, squares, linewidth=3)
 
