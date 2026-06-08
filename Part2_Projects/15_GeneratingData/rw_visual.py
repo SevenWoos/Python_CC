cfe1613b05c8e42e9f4e1a1a8fcc89e3a20c38e4
SevenWoos <swenwu96@gmail.com>
2026-06-07 19:12:03 -0700

    Hiding the axes so they don't distract from the path of each random walk.

--- Part2_Projects/15_GeneratingData/rw_visual.py
+++ Part2_Projects/15_GeneratingData/rw_visual.py
@@ -20,6 +20,10 @@ while True:
   ax.scatter(0, 0, c='green', edgecolors='none', s=100)
   ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
   
+  # Remove the axes so they don't distract from the path of each walk.
+  ax.get_xaxis().set_visible(False)
+  ax.get_yaxis().set_visible(False)
+  
   plt.show()
   
   keep_running = input("Make another walk? (y/n): ")
