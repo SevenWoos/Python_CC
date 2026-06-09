f5639dfcd7fa69c3f3e63f1bbfa87296afa3f677
SevenWoos <swenwu96@gmail.com>
2026-06-08 22:39:20 -0700

    Saving the d6_d10 plot to an html file.

--- Part2_Projects/15_GeneratingData/plotly/dice_visual_d6d10.py
+++ Part2_Projects/15_GeneratingData/plotly/dice_visual_d6d10.py
@@ -27,4 +27,7 @@ fig = px.bar(x=poss_results, y=frequencies, title=title, labels=labels)
 
 fig.update_layout(xaxis_dtick=1)
 
-fig.show()
+# fig.show()
+
+# Saving the figure.
+fig.write_html('dice_visual_d6d10.html')
