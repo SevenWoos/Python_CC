02b2ed9baee6bb2d35502927f6c4b5fa26f0289e
SevenWoos <swenwu96@gmail.com>
2026-06-09 16:34:24 -0700

    Updating the xdate auto format to draw the dates diagonally for death valley weather data.

--- Part2_Projects/16_DownloadingData/death_valley_highs_lows.py
+++ Part2_Projects/16_DownloadingData/death_valley_highs_lows.py
@@ -38,6 +38,7 @@ ax.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 title = "Daily High and Low Temperatures, 2021 \nDeath Valley, CA"
 ax.set_title(title, fontsize=20)
 ax.set_xlabel('', fontsize=16)
+fig.autofmt_xdate()
 ax.set_ylabel("Temperature (F)", fontsize=16)
 ax.tick_params(labelsize=16)
 
