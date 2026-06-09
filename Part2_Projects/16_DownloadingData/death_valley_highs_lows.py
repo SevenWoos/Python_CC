a3134509dce0177d08e88a295519634b00aedf42
SevenWoos <swenwu96@gmail.com>
2026-06-09 13:38:24 -0700

    Adding error handling for missing dates for Death Valley temperatures graph.

--- Part2_Projects/16_DownloadingData/death_valley_highs_lows.py
+++ Part2_Projects/16_DownloadingData/death_valley_highs_lows.py
@@ -15,11 +15,16 @@ dates, highs, lows = [], [], []
 
 for row in reader:
   current_date = datetime.strptime(row[2], '%Y-%m-%d')
-  high = int(row[3])
-  low = int(row[4])
-  dates.append(current_date)
-  highs.append(high)
-  lows.append(low)
+  try:
+    
+    high = int(row[3])
+    low = int(row[4])
+  except ValueError:
+    print(f"Missing data for {current_date}")
+  else:
+    dates.append(current_date)
+    highs.append(high)
+    lows.append(low)
   
 # Plot the high and low temperatures.
 plt.style.use('seaborn-v0_8')
